50d162e48bf0d6632236ebcbd474aedfca39e4d9
gowrishnkar <gowrishnkar@gmail.com>
2016-10-13 13:33:18 +0530

    SearchBox functionality in Listview

--- KWIDF-UI/app/shared/filter-listview/filter-listview.component.ts
+++ KWIDF-UI/app/shared/filter-listview/filter-listview.component.ts
@@ -19,11 +19,12 @@ export class FilterListViewComponent {
     subscription: Subscription;
     ObjFilter: TreeViewFilter;
     page: number = 1;
-
+    localFilters: ListViewFilter[];
     constructor(private filterListviewDataService: ListViewDataService,private filterDataService: FilterDataService) { }
 
     getListviewFilters(cFilters: Array<number>) {
         this.filters = this.filterListviewDataService.getListviewFilters(cFilters);
+        this.localFilters = this.filterListviewDataService.getListviewFilters(cFilters);
     }
 
     ngOnInit(): void {      
@@ -39,6 +40,25 @@ export class FilterListViewComponent {
                 this.getListviewFilters(cFilters);
                 this.page = 1;
             });
+    }
+    OnSearch(newItem: string) {       
+        if (newItem != "") {
+            this.filters = this.localFilters.filter(
+                x => x.wellName.toLowerCase().indexOf(newItem.toLowerCase())!=-1);
+        }
+        else {
+            this.filters = this.localFilters;
+        }
+       
+    }
+    eventHandler(event: any) {
+        if (event.target.value != "") {
+            this.filters = this.localFilters.filter(
+                x => x.wellName.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1);
+        }
+        else {
+            this.filters = this.localFilters;
+        }
     }
     
 }
